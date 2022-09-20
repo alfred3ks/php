50dd883596070aa2280cff364d267b416f79230c
Alfredo Sánchez <alfred3ks@gmail.com>
2022-09-20 19:05:36 +0200

    Variables númericas.

---
+++ 8-variables-numericas.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Variables numericas.</title>
+</head>
+
+<body>
+  <?php
+
+  /*
+  Vamos a ver ahora las variables numericas, tanto los enteros como los decimales.
+  Las variables numericas no van englobadas en comillas simples o dobles.
+  */
+
+  // Numero entero:
+  $numeroEntero = -123;
+  $numInteger = 456;
+
+  // Decimales: OJO el punto en ves de coma:
+  $numDecimal = 345.78;
+
+  ?>
+</body>
+
+</html>
