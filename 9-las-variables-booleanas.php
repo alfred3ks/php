bcf1b58b6b34d926fa4769f55adbf151d2c58542
Alfredo Sánchez <alfred3ks@gmail.com>
2022-09-20 19:10:05 +0200

    Los booleanos.

---
+++ 9-las-variables-booleanas.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Las variables Booleanas</title>
+</head>
+
+<body>
+  <?php
+
+  /*
+
+  Vamos a ver ahora las variables booleanas.
+
+  Sirven para almacenar información lógica. Solo pueden tomas dos valores, true o false.
+  Suelen ser muy utiles cuando usamos condicionales.
+
+  */
+
+  $varBool = true;
+  $varBool = false;
+
+  ?>
+</body>
+
+</html>
