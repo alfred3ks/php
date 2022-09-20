ce707d1bbb5e5adf6c76731b8b0b1dba98d2ffca
Alfredo Sánchez <alfred3ks@gmail.com>
2022-09-20 19:36:14 +0200

    Operadores aritmeticos básicos.

---
+++ 11-operadores-aritmeticos.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Operadores Aritmeticos</title>
+</head>
+
+<body>
+  <?php
+
+  /*
+
+  Vamos a ver los operadores aritmeticos basicos:
+  -sumas,
+  -resta,
+  -multiplicacion,
+  -division,
+  -modulo
+
+  */
+
+  $a = 100;
+  $b = 2.30;
+
+  // Suma:
+  $suma = $a + $b;
+  echo $suma;
+  echo "<br>";
+  // Resta:
+  $resta = $a - $b;
+  echo $resta;
+  echo "<br>";
+  // Multiplicacion:
+  $multi = $a * $b;
+  echo $multi;
+  echo "<br>";
+  // Division:
+  $div = $a / $b;
+  echo $div;
+  echo "<br>";
+
+  // Modulo:
+  $mod = $a % $b;
+  echo $mod;
+
+  ?>
+</body>
+
+</html>
