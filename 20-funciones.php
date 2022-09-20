70ab41de80c83804f280e4692e1e18ceb47aec10
Alfredo Sánchez <alfred3ks@gmail.com>
2022-09-20 23:00:55 +0200

    Las funciones en PHP.

---
+++ 20-funciones.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Las funciones.</title>
+</head>
+
+<body>
+  <h1>Funciones</h1>
+  <?php
+  /*
+
+  Ahora vamos a ver el tema de las funciones.
+  Son un conjunto de instrucciones que se almacena para posteriormente poder ser ejecutas por medio de una llamada.
+
+  Existen dos tipo de funciones, las existentes del lenguaje y las propias.
+  Vamos a ver las de PHP, que incorpora mas de 100 funciones.
+
+
+  */
+  ?>
+</body>
+
+</html>
