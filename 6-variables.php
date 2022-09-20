2294e151f6a98439dd5f3d35dec6f398ea7290e0
Alfredo Sánchez <alfred3ks@gmail.com>
2022-09-20 18:19:47 +0200

    Las variables.

---
+++ 6-variables.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Vamos a ver la variables en PHP:
+Veremos que es una variable y que tipos hay.
+
+¿Que es una variable?
+- Son un elemento clave en todos los lenguajes de programacion.
+- Permiten almacenar valores.
+
+En PHP existen distintos tipos de variables:
+- Cadenas de caracteres, (String):
+"Mi gato Garfiel"
+
+- Numeros enteros, (int):
+21, 45,3, 67, 100, -45, -7
+
+- Numeros decimales, (float) Se expresan con un punto y no coma:
+3.14, 0.156, 12.45, etc
+
+- Booleanos, (boo):
+true o false
+
+- De tipo null:
+Esta vacio
+
+Estos son los básicos.
+
+*/
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Las variables en PHP</title>
+</head>
+
+<body>
+  <?php ?>
+</body>
+
+</html>
