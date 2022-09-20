f81f8d81861391b6c352556aea3150da29e565d8
Alfredo Sánchez <alfred3ks@gmail.com>
2022-09-20 17:51:20 +0200

    Inserción de PHP dentro del HTML.

---
+++ 3-insercion-HTML.php
@@ -0,0 +1,33 @@
+<!--
+Vamos a ver como insertar codigo PHP dentro de HTML.
+Normalmente lo que se hace es insertar el codigo de php dentro del propio HTML usando las etiquetas:
+
+<?php ?>
+
+De esta forma el código estático se convierte en dinámico.
+Ahora algo que debes saber es que siempre al final de la instrucción debes colocar punto y coma.
+Lo vemos mas abajo.
+
+-->
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mi primera página con PHP</title>
+</head>
+
+<body>
+  <!-- Aqui incluimos nuestro codigo php -->
+  <?php
+
+  // Aqui nuestro codigo PHP.
+
+  ?>
+
+</body>
+
+</html>
