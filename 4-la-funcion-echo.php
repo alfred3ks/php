2057e0cc475dab92f09f870c5a02aecf26b61f78
Alfredo Sánchez <alfred3ks@gmail.com>
2022-09-20 17:56:31 +0200

    La funcion echo.

---
+++ 4-la-funcion-echo.php
@@ -0,0 +1,30 @@
+<!--
+Vamos a ver la funcion echo.
+
+-->
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Función echo</title>
+</head>
+
+<body>
+
+  <?php
+
+  // Mostramos una cadena de texto:
+  echo "Hola me llamo Pepito";
+
+  // Mostramos una cadena de texto con codigo HTML:
+  echo "<p>Esto es un texto dentro de etiquetas <strong>HTML</strong>.</p>";
+
+  ?>
+
+</body>
+
+</html>
